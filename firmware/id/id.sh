1a733ef4d152241c9f3d56b71ab823b6c75ceec2
Danny Chou <danchouzhou@gmail.com>
2022-06-07 13:13:59 +0800

    Create Objects folder

--- firmware/id/id.sh
+++ firmware/id/id.sh
@@ -2,6 +2,10 @@
 
 # Generate CAN ID bianry file from shell script
 
+mkdir -p ./Objects
+cd ./Objects
+rm -f *.bin
+
 for i in {0..2047}
 do
     echo -n -e "\x$(printf '%02x' $((i & 0xFF)))\x$(printf '%02x' $(((i & 0xFF00) >> 8)))" > $i.bin
