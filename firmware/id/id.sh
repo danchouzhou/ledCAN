008d9b6a790476092197e52197c92fa41ab489a5
Danny Chou <danchouzhou@gmail.com>
2022-06-20 18:38:46 +0800

    Fill the high two bytes in zero

--- firmware/id/id.sh
+++ firmware/id/id.sh
@@ -8,5 +8,6 @@ rm -f *.bin
 
 for i in {0..2047}
 do
-    echo -n -e "\x$(printf '%02x' $((i & 0xFF)))\x$(printf '%02x' $(((i & 0xFF00) >> 8)))" > $i.bin
+#   FMC_Read() read four bytes and return in a uint32_t
+    echo -n -e "\x$(printf '%02x' $((i & 0xFF)))\x$(printf '%02x' $(((i & 0xFF00) >> 8)))\x00\x00" > $i.bin
 done
